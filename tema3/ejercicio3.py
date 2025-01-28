6d62dd1cc7e384606ff769018dadf02c6da4765a
franastorGana <99664443+franastorGana@users.noreply.github.com>
2025-01-28 12:01:48 +0000

    s

---
+++ tema3/ejercicio3.py
@@ -0,0 +1,27 @@
+'''
+Ejercicio con operaciones numéricas y booleanas
+
+Definir la expresión que devuelve si:
+
+5 se encuentra en la lista formada por los números 2, 3, 4 y 5
+
+
+Y además:
+
+Si 1 entre la raíz cuadrada de 2 es igual 0.25
+
+
+El resultado final debe ser True
+
+'''
+# primero definimos la lista
+
+numeros = [2, 3, 4, 5]
+
+comprobar = 5
+
+#ahora comprobamos si el numero 5 esta en la lista
+
+indice = numeros.index(comprobar)
+
+print(comprobar)
