88be51c4878aed6773891c8ed3106453a3295bc1
franastorGana <99664443+franastorGana@users.noreply.github.com>
2025-01-28 11:28:07 +0000

    ejercicio1 tema 3

---
+++ tema3/ejercicio1.py
@@ -0,0 +1,27 @@
+'''
+Ejercicio balance de gastos
+
+Definir la expresión para calcular el balance de ingresos y gastos donde:
+
+Ingresos Totales = 2000.60 euros
+
+Gasto1 = 500.30 euros
+
+Gasto2 = 400.20 euros
+
+Gasto3 = 100.10 euros
+
+El resultado final debe ser igual a 1000.0
+'''
+
+# Ingresos totales
+ingresos_totales = 2000.60
+# Gasto 1
+gasto1 = 500.30
+# Gasto 2
+gasto2 = 400.20
+# Gasto 3
+gasto3 = 100.10
+# Calcular el balance de ingresos y gastos
+balance = ingresos_totales - gasto1 - gasto2 - gasto3
+print(balance)
