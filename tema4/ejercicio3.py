46f2e3f69f4431c2380d5639630ea428f4c052cf
franastorGana <99664443+franastorGana@users.noreply.github.com>
2025-01-29 14:01:20 +0000

    ejercicio3

---
+++ tema4/ejercicio3.py
@@ -0,0 +1,16 @@
+'''
+Realizar las siguientes operaciones:
+
+Pedir por consola el año de nacimiento
+Calcular la edad
+Mostrar con un print la edad calculada
+
+'''
+
+import datetime
+# Obtener el año de nacimiento del usuario
+anio_nacimiento = int(input("Ingrese tu año de nacimiento: "))
+anio_actual = datetime.datetime.now().year
+edad = anio_actual - anio_nacimiento
+
+print("Tu edad es:", edad)
