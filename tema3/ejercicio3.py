5d7a2e73f53037841190e0aa00db0203a4ad45f5
franastorGana <99664443+franastorGana@users.noreply.github.com>
2025-01-29 09:59:01 +0000

    acabamos el ejercicio3

--- tema3/ejercicio3.py
+++ tema3/ejercicio3.py
@@ -23,5 +23,22 @@ comprobar = 5
 #ahora comprobamos si el numero 5 esta en la lista
 
 indice = numeros.index(comprobar)
-
 print(comprobar)
+print(indice)
+resultado = comprobar == indice
+
+print(resultado)
+
+#comprobar 5 se encuentra en la lista formada por los números 2, 3, 4 y 5
+
+lista = [2, 3, 4, 5]
+#ahora comprobamos si el numero 5 esta en la lista
+indice = lista.index(5)
+numero = lista[indice]
+#si esta devolvemos un true
+resultado = numero == 5
+print(resultado)
+
+#Si 1 entre la raíz cuadrada de 2 es igual 0.25
+resultado = 1 / (2**0.5) == 0.25
+print(resultado)
