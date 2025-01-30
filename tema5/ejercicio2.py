79b2cf4bab5b69900e1745bdfbec6dfa14424ce6
franastorGana <99664443+franastorGana@users.noreply.github.com>
2025-01-30 10:13:33 +0000

    arreglamos else

--- tema5/ejercicio2.py
+++ tema5/ejercicio2.py
@@ -4,7 +4,7 @@ Dada la siguiente lista de números enteros:
 
 lista_origen = [1,45,4,78,23,2,0,67]
 
-Obtener una nueva lista con dos elementos, donde:
+Obtener una nueva lista con dos elementos, donde: 
 El primer elemento esté formado por los números pares de lista_origen
 El segundo elemento esté formado por los números impares de lista_origen
 '''
@@ -14,9 +14,8 @@ lista_impares  = []
 for numero in lista_origen:
   if numero % 2 == 0 : 
     lista_pares.append(numero)
-  else
+  else : 
     lista_impares.apped(numero)
 
 print(lista_pares)
 print(lista_impares)
-
