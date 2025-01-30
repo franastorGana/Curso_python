1627533457420cac442402d135a9e71a04c9f13c
franastorGana <99664443+franastorGana@users.noreply.github.com>
2025-01-30 10:06:54 +0000

    ejercicio1 tema5

---
+++ tema5/ejercicio1.py
@@ -0,0 +1,25 @@
+'''
+Ejercicio sobre variables globales y locales
+
+Sobre el siguiente extracto de código, introduce las sentencias necesarias dentro de la funcion my_function_1 para que el valor de a que muestre el print sea de 25.
+
+Ejercicio 1
+
+a = 20
+
+def my_funcion_1():
+……….
+……….
+
+my_funcion_1()
+print(a)
+
+'''
+a = 20
+
+def my_funcion_1():
+global a
+a = 25
+
+my_funcion_1()
+print(a)
