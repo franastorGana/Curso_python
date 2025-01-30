8f8fb591bebc15bda9b5cb48b6865c6ba935370c
franastorGana <99664443+franastorGana@users.noreply.github.com>
2025-01-30 10:11:42 +0000

    tema 5 ejercicio 2

---
+++ tema5/ejercicio2.py
@@ -0,0 +1,22 @@
+'''
+Dada la siguiente lista de números enteros:
+
+
+lista_origen = [1,45,4,78,23,2,0,67]
+
+Obtener una nueva lista con dos elementos, donde:
+El primer elemento esté formado por los números pares de lista_origen
+El segundo elemento esté formado por los números impares de lista_origen
+'''
+lista_origen = [1,45,4,78,23,2,0,67]
+lista_pares = []
+lista_impares  = []
+for numero in lista_origen:
+  if numero % 2 == 0
+    lista_pares.append(numero)
+  else
+    lista_impares.apped(numero)
+
+print(lista_pares)
+print(lista_impares)
+
