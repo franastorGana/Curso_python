f2768a397609d774098f06b030ed1ee5ce2915fe
franastorGana <99664443+franastorGana@users.noreply.github.com>
2025-01-30 10:12:35 +0000

    arreglamos el if

--- tema5/ejercicio2.py
+++ tema5/ejercicio2.py
@@ -12,7 +12,7 @@ lista_origen = [1,45,4,78,23,2,0,67]
 lista_pares = []
 lista_impares  = []
 for numero in lista_origen:
-  if numero % 2 == 0
+  if numero % 2 == 0 : 
     lista_pares.append(numero)
   else
     lista_impares.apped(numero)
